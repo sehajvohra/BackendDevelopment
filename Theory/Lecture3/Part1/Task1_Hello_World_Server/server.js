11ef4f2915bd9b87f4cb85701675047b30a7b275
Sehaj Vohra <sehajvohra04@gmail.com>
2026-08-21 20:26:05 +0530

    Add Lecture 3 Part 1 tasks 1 and 3

--- Theory/Lecture3/Part1/Task1_Hello_World_Server/server.js
+++ Theory/Lecture3/Part1/Task1_Hello_World_Server/server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Backend Server Running");
+  res.send("Backend Server Running");
 });
 
 app.listen(3000, () => {
-    console.log("Server started at http://localhost:3000");
+  console.log("Server started at http://localhost:3000");
 });
